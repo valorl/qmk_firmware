c8fec604e73b2f58ee348fb99df4bcd2c9f9bc5a
valorl <11498571+valorl@users.noreply.github.com>
2022-01-25 12:16:42 +0100

    update?

--- keyboards/bastardkb/scylla/keymaps/valorl/config.h
+++ keyboards/bastardkb/scylla/keymaps/valorl/config.h
@@ -1,14 +1,19 @@
 // https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-term
 #undef TAPPING_TERM
-#define TAPPING_TERM 180
+#define TAPPING_TERM 100
+
+#define TAPPING_TERM_PER_KEY
 
 // https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
+// #undef IGNORE_MOD_TAP_INTERRUPT
 #define IGNORE_MOD_TAP_INTERRUPT
 // https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#tapping-term
-#define PERMISSIVE_HOLD
+//#define PERMISSIVE_HOLD
 
 // Must be plugged as defined here
 #undef MASTER_RIGHT
 #undef MASTER_LEFT
 #define MASTER_LEFT
+
+#define PREVENT_STUCK_MODIFIERS
 
--- keyboards/bastardkb/scylla/keymaps/valorl/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/valorl/keymap.c
@@ -15,12 +15,24 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdio.h>
 #include QMK_KEYBOARD_H
+
+
+#define TAPPING_TERM_INCREMENT 10
 
 enum layers {
     BASE,
     SYMBOLS
 };
+
+enum custom_dynamic_tapping_term_keys {
+    DT_PRNT = SAFE_RANGE,
+    DT_UP,
+    DT_DOWN,
+};
+
+uint16_t g_tapping_term = TAPPING_TERM;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,13 +56,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                '------+------'         '------+------'
  */
   [0] = LAYOUT_split_4x6_5(
-    KC_GRV,               KC_1, KC_2, KC_3, KC_4, KC_5,	      KC_6, KC_7, KC_8,    KC_9,   KC_0,                  KC_MINS,
-    KC_TAB,               KC_Q, KC_W, KC_E, KC_R, KC_T,	      KC_Y, KC_U, KC_I,    KC_O,   KC_P,                  KC_BSLS,
-    MT(MOD_LSFT, KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G,	      KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, MT(MOD_RSFT ,KC_QUOT),
-    KC_LSFT,              KC_Z, KC_X, KC_C, KC_V, KC_B,       KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,               KC_RSFT,
+    KC_GRV,                            KC_1, KC_2, KC_3, KC_4, KC_5,	      KC_6, KC_7, KC_8,    KC_9,   KC_0,                  KC_MINS,
+    KC_TAB,                            KC_Q, KC_W, KC_E, KC_R, KC_T,	      KC_Y, KC_U, KC_I,    KC_O,   KC_P,                  KC_BSLS,
+    KC_ESC,                            KC_A, KC_S, KC_D, KC_F, KC_G,	      KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,               KC_QUOT,
+    KC_LSFT,              MT(MOD_LSFT,KC_Z), KC_X, KC_C, KC_V, KC_B,          KC_N, KC_M, KC_COMM, KC_DOT, MT(MOD_RSFT,KC_SLSH),  KC_RSFT,
 
-                                KC_SPC, MO(1), KC_LGUI,       MO(1), KC_ENT, KC_BSPC,
-                                      KC_LCTL, KC_LALT,       _______, KC_RCTL
+                                    KC_SPC, LCTL_T(KC_SPC), KC_LALT,          KC_RALT, LT(1, KC_ENT), KC_BSPC,
+                                                   KC_LCTL, KC_LGUI,          KC_LGUI, KC_RCTL
   ),
 
 /* Symbol layer
@@ -75,9 +87,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, KC_END,  _______,	    KC_HOME, _______, _______, _______, _______,  _______,
     KC_PAUS, _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_PLUS,		KC_EQL,  KC_RPRN, KC_RBRC, KC_RCBR, _______,  _______,
     _______, _______, _______, _______, _______, _______,	    KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______,  _______,
-    _______, _______, _______, _______, _______, _______,       _______, _______, _______, _______, _______,  _______,
+    _______, _______, _______, DT_PRNT, DT_DOWN, DT_UP,       _______, _______, _______, _______, _______,  _______,
 
                                _______, _______, _______,		_______, _______, _______,
                                         _______, _______,       _______, _______
   ),
 };
+
+
+
+static void tapping_term_report(void) {
+    char display[8];
+    snprintf(display, sizeof(display), "%d", g_tapping_term);
+    send_string((const char *)display);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case DT_PRNT:
+        if (record->event.pressed) {
+            tapping_term_report();
+        }
+        break;
+    case DT_DOWN:
+        if (record->event.pressed) {
+            g_tapping_term -= TAPPING_TERM_INCREMENT;
+        }
+        break;
+    case DT_UP:
+        if (record->event.pressed) {
+            g_tapping_term += TAPPING_TERM_INCREMENT;
+        }
+        break;
+    }
+    return true;
+};
+
+
+
+/* uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) { */
+/*     switch (keycode) { */
+/*         default: */
+/*            return g_tapping_term; */
+/*     } */
+/* } */
